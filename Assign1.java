7c5ce9eb76879d3e92f414fe1e91c91822b2f434
Pratik Pund <88658436+pundpratik@users.noreply.github.com>
2021-09-22 14:47:42 +0530

    Add files via upload

---
+++ Assign1.java
@@ -0,0 +1,24 @@
+//accept two integer values via CLA* and perform all arithmetic operation
+import java.util.Scanner;
+class Assign1
+{
+	public static void main(String []args)
+	{
+		
+		int num1;
+		int num2;
+		//Scanner in=new Scanner(System.in);
+		//System.out.println("enter a number");
+		int num1=in.nextInt();
+		System.out.println("enter a number");
+		int num2=in.nextInt();
+		int sum=num1+num2;
+		int sub=num1-num2;
+		int mul=num1*num2;
+		int div=num1/num2;
+		System.out.println(" addition of two number is="+sum);
+		System.out.println(" addition of two number is="+sub);
+		System.out.println(" addition of two number is="+mul);
+		System.out.println(" addition of two number is="+div);
+	}
+}
